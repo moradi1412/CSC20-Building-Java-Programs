604b82422c8a2648033212e0a9c561f634178140
Moradi1412 <moradi.javid@gmail.com>
2022-10-12 16:22:19 -0700

    new file explaining the superclass Object and inheritance

---
+++ Week 6/student.java
@@ -0,0 +1,37 @@
+import java.util.*; 
+
+public class student {
+    private int id; 
+    public student (int id){
+        this.id = id; 
+    }
+    public String toString() {
+        return super.toString() + "\nStudent id : " + id ; 
+    }
+    // public boolean equals(student s){
+    //     return this.id == s.id; 
+    // }
+    
+}
+
+class driver {
+    public static void main(String[] args) {
+        student s1 = new student(123); 
+        student s2 = new student(123); 
+        boolean b = s1.equals(s2); 
+        System.out.println(b);
+        //if the boolan is not defined method , then the application will use the object method 
+        //boolean equals where it will compare the reference memory of two obj created. 
+        System.out.println(s1.toString());
+     
+
+        Object o;
+        //as Object is the superclass in any java application, you can access through Object and create new subclass 
+        o = new student(786); 
+        o = "hello" ; 
+        o = new Random();  
+
+        
+    }
+
+}
