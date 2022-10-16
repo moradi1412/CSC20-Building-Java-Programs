557cca38c483f8b72adf5de6a4e5e2a91e2c4b10
Moradi1412 <moradi.javid@gmail.com>
2022-10-16 12:29:38 -0700

    introduction to arraylist

---
+++ Week 7/IntroArrayList.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class IntroArrayList {
+    public static void main(String[] args) {
+        //ArrayList Declaration 
+        ArrayList<String> names = new ArrayList<String>(); 
+        names.add("Marry", null);
+        String [] list = new String [20]; 
+    }
+    
+}
