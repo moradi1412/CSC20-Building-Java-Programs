db9b36399ed2aba8af1220c0856c50b6c6685ced
Moradi1412 <moradi.javid@gmail.com>
2022-10-16 12:31:19 -0700

    index adding

--- Week 7/IntroArrayList.java
+++ Week 7/IntroArrayList.java
@@ -4,7 +4,9 @@ public class IntroArrayList {
     public static void main(String[] args) {
         //ArrayList Declaration 
         ArrayList<String> names = new ArrayList<String>(); 
-        names.add("Marry", null);
+        names.add("Marry");
+        //shifting element using index in function add 
+        names.add(0, "Alex") 
         String [] list = new String [20]; 
     }
     
