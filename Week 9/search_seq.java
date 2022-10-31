719f514401a1223b0c24aa3912867fa46947697b
Moradi1412 <moradi.javid@gmail.com>
2022-10-31 14:22:48 -0700

    search seqence added

---
+++ Week 9/search_seq.java
@@ -0,0 +1,19 @@
+
+
+public class search_seq
+{
+
+    public static void main(String[] args) {
+        int [] list = {45, 23, 56, 22, 88, 54,32,45,99, 453 }; 
+        int index = search(list, 453);  
+        System.out.println(index);
+    }
+
+    public static int search(int[] list, int num ) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] == num )
+            return i; 
+        }
+        return -1; 
+    }
+}
